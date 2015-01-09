9a8e4645a8257cebedc3f5d23463f8b62e175311
Peter Prokop <prokop.petr@gmail.com>
2015-01-09 22:22:44 +0300

    First commit

--- Shipio-test/Shipio-testTests/Shipio_testTests.m
+++ Shipio-test/Shipio-testTests/Shipio_testTests.m
@@ -8,6 +8,7 @@
 
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
+#import "ViewController.h"
 
 @interface Shipio_testTests : XCTestCase
 
@@ -33,7 +34,8 @@
 - (void)testPerformanceExample {
     // This is an example of a performance test case.
     [self measureBlock:^{
-        // Put the code you want to measure the time of here.
+        ViewController* vc = [[ViewController alloc] init];
+        [vc loadView];
     }];
 }
 
